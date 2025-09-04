50d14f5e55055d443742997b158a43ca865bb5ee
sofiabns <sofiabns@gmail.com>
2025-09-04 12:51:46 -0300

    arquivos deletados

--- utils/file_manager.py
+++
@@ -1,6 +0,0 @@
-import os
-from config import SOURCE_FOLDER
-
-def list_files():
-    """Lista todos os arquivos da pasta de origem"""
-    return [os.path.join(SOURCE_FOLDER, f) for f in os.listdir(SOURCE_FOLDER)]
